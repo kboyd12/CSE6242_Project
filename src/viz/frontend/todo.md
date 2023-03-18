c1b9d4864caa587d71deb350bd45f39dff5ae43d
kboyd12 <ktboyd43@gmail.com>
2023-03-18 18:15:05 -0400

    Created the initial plotly map. Need to add a trace for lines stemming from the origin location now based on our data

---
+++ src/viz/frontend/todo.md
@@ -0,0 +1,5 @@
+# Frontend
+
+- Download GeoJSON for the US
+- ~~Scrape the data for unique airports and download/make a GeoJSON for those locations~~
+- 
---
+++ src/viz/frontend/view.py
@@ -0,0 +1,37 @@
+from ...data.get_airport_locations import get_airport_locations
+import plotly.graph_objects as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+def create_plot():
+    df = get_airport_locations()
+
+    fig = go.Figure()
+
+    fig.add_trace(
+        go.Scattergeo(
+            locationmode="USA-states",
+            lon=df.longitude_deg,
+            lat=df.latitude_deg,
+            hoverinfo="text",
+            text=df.name,
+            mode="markers",
+            marker=dict(size=2, color="rgb(255,0,0)"),
+        )
+    )
+
+    fig.update_layout(
+        title_text="Placeholder Title for Now",  # TODO
+        showlegend=False,
+        geo=dict(
+            scope="north america",
+            projection_type="azimuthal equal area",
+            showland=True,
+            landcolor="rgb(243,243,243)",
+            countrycolor="rgb(204,204,204)",
+        ),
+    )
+
+    return fig
