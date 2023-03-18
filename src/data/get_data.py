5ced7381015d3bb3d19f7bedb02d435f8196b307
kboyd12 <ktboyd43@gmail.com>
2023-03-18 19:37:43 -0400

    Updated requirements; Turned on progress bar and formatted with Black in the get data script

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 import kaggle
-import os 
+import os
 
-def get_data(outdir: str="data/raw") -> None:
+
+def get_data(outdir: str = "data/raw") -> None:
     """downloads our datasets off kaggle into data"""
-    
+
     def dl_from_kaggle(dataset: str, datasetname: str, outdir: str) -> None:
         writepath = os.path.join(os.path.abspath(outdir), datasetname)
         os.makedirs(writepath, exist_ok=True)
         kaggle.api.authenticate()  # Note: if failure here, check README for auth directions
-        kaggle.api.dataset_download_files(dataset, path=writepath, unzip=True)
+        kaggle.api.dataset_download_files(
+            dataset, path=writepath, unzip=True, quiet=False
+        )
 
     # delay dataset
-    dl_from_kaggle('giovamata/airlinedelaycauses', 'delay', outdir)
-    dl_from_kaggle('robikscube/flight-delay-dataset-20182022', 'btsdelay', outdir)
+    dl_from_kaggle("giovamata/airlinedelaycauses", "delay", outdir)
+    dl_from_kaggle("robikscube/flight-delay-dataset-20182022", "btsdelay", outdir)
 
     # delete the huge, redundant csv files
-    dir_name = os.path.abspath(os.path.join(outdir, 'btsdelay'))
+    dir_name = os.path.abspath(os.path.join(outdir, "btsdelay"))
     files = os.listdir(dir_name)
 
     for file in files:
         if file.endswith(".csv"):
             os.remove(os.path.join(dir_name, file))
+
 
 if __name__ == "__main__":
     get_data()
