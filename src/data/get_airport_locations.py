84ae1baecc72ad2a49010a1e2cba1b5d7a56e79c
kboyd12 <ktboyd43@gmail.com>
2023-03-18 17:50:20 -0400

    Added in a loader if the file already exists to cut down on time; reset the index of the csv as well

--- src/data/get_airport_locations.py
+++ src/data/get_airport_locations.py
@@ -5,18 +5,30 @@ from pathlib import Path
 def get_airport_locations(
     indir: str = "data/raw/btsdelay", outdir: str = "data/raw/locations"
 ) -> pd.DataFrame:
-    LOCATION_URI = "https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv"
+    """Grabs a csv database of airport locations from github and matches the ones in our dataset"""
+    LOCATION_URI = "https://davidmegginson.github.io/ourairports-data/airports.csv"
+    curr_path = Path().absolute()
+    outfile = curr_path.joinpath(outdir).joinpath("locations.csv")
 
-    curr_path = Path()
-    flight_data_path = curr_path.absolute().joinpath(indir)
+    if outfile.exists():
+        return pd.read_csv(outfile)
 
+    flight_data_path = curr_path.joinpath(indir)
     flight_df = pd.concat(
         [pd.read_parquet(i) for i in flight_data_path.glob("*.parquet")]
     )
 
-    port_origin_list: list = flight_df.Origin.unique()
+    port_origin_list = flight_df.Origin.unique()
     port_locations = pd.read_csv(LOCATION_URI)
 
-    locations_df = port_locations[port_locations.iata_code.isin(port_origin_list)]
+    locations_df: pd.DataFrame = port_locations[
+        port_locations.iata_code.isin(port_origin_list)
+    ].reset_index(drop=True)
+
+    locations_df.to_csv(outfile)
 
     return locations_df
+
+
+if __name__ == "__main__":
+    get_airport_locations()
